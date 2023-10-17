d0eb7e28c097d93950bb6e062dfc3c6fe436a0cb
Jens Ettl <ettljens@gmail.com>
2023-10-17 15:12:02 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 100-days-of-code-python
+
+## Table of Contents
+
+### Day 1: Variables
+
+_lorem ipsum_
+
+### Day 2: Data
---
+++ day01-variables/main.py
@@ -0,0 +1,24 @@
+# String concatenation
+print("Hello" + "World")
+
+# Escape characters \
+print("Hello\nWorld")
+print("print('Hello World')")
+
+# input function
+print("Hello " + input("What is your name? "))
+
+# len function
+name = "Jack"
+print(len(name))
+
+# Exercise 1 - Switching Values
+a = input("a: ")
+b = input("b: ")
+## Code below
+
+a, b = b, a
+
+# Code above
+print("a: " + a)
+print("b: " + b)
