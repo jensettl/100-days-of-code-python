67463852dd92720f92eb709c699fd05aa84b8fa8
Jens Ettl <ettljens@gmail.com>
2023-10-23 22:01:21 +0200

    added day23

---
+++ day23-TurtleCrossing/car_manager.py
@@ -0,0 +1,32 @@
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager:
+    def __init__(self):
+        self.all_cars = []
+
+    def create_car(self):
+        random_chance = random.randint(1, 5)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_len=2, stretch_wid=1)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            new_car.setposition(random.randint(300, 600), random.randint(-250, 250))
+            self.car_speed = STARTING_MOVE_DISTANCE
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def respawn(self):
+        self.setposition(random.randint(300, 600), random.randint(-250, 250))
+
+    def increase_car_speed(self):
+        self.car_speed *= 1.2
